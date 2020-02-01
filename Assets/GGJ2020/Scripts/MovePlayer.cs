1ae1b04a9f024c6095098aa036584a158e8926e0
Andrew McPherson <ehgoodenough@gmail.com>
2020-02-01 09:19:34 -0800

    Added a player with a movement script.

---
+++ Assets/GGJ2020/Scripts/MovePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // TODO: Load these from a file!!
+    public string upKey = "w";
+    public string leftKey = "a";
+    public string rightKey = "d";
+    public string downKey = "s";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(this.upKey))
+        {
+            Debug.Log(this.upKey);
+            this.transform.position += Vector3.up;
+        }
+        if(Input.GetKeyDown(this.downKey))
+        {
+            Debug.Log(this.downKey);
+            this.transform.position += Vector3.down;
+        }
+        if(Input.GetKeyDown(this.leftKey))
+        {
+            Debug.Log(this.leftKey);
+            this.transform.position += Vector3.left;
+        }
+        if(Input.GetKeyDown(this.rightKey))
+        {
+            Debug.Log(this.rightKey);
+            this.transform.position += Vector3.right;
+        }
+    }
+}
