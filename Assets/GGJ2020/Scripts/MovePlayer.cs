8af52c4c452282642d51b547947448a60be6d16d
chaosed0 <chaosed0@gmail.com>
2020-02-01 11:09:05 -0800

    Add simple kvp parser & key remapping

--- Assets/GGJ2020/Scripts/MovePlayer.cs
+++ Assets/GGJ2020/Scripts/MovePlayer.cs
@@ -4,37 +4,61 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const string keyBindingPath = "Data/keys.data";
+
+    private string upKey = null;
+    private string leftKey = null;
+    private string rightKey = null;
+    private string downKey = null;
+
     void Start()
     {
-
+        LoadKeyBindings();
     }
 
-    // TODO: Load these from a file!!
-    public string upKey = "w";
-    public string leftKey = "a";
-    public string rightKey = "d";
-    public string downKey = "s";
+    private void LoadKeyBindings()
+    {
+        Dictionary<string, string> keyBindings = KeyValueFormatParser.Parse(MetaLoadUtil.GetPath(keyBindingPath));
+
+        if (keyBindings == null)
+        {
+            Debug.LogError("Key bindings file not found!");
+            return;
+        }
+
+        MapKey(keyBindings, "up", out upKey);
+        MapKey(keyBindings, "down", out downKey);
+        MapKey(keyBindings, "left", out leftKey);
+        MapKey(keyBindings, "right", out rightKey);
+    }
+
+    private void MapKey(Dictionary<string, string> map, string key, out string value)
+    {
+        if (!map.TryGetValue(key, out value))
+        {
+            Debug.LogError($"Key '{key}' not found in keys.data!");
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(this.upKey))
+        if (this.upKey != null && Input.GetKeyDown(this.upKey))
         {
             Debug.Log(this.upKey);
             this.transform.position += Vector3.up;
         }
-        if(Input.GetKeyDown(this.downKey))
+        if (this.downKey != null && Input.GetKeyDown(this.downKey))
         {
             Debug.Log(this.downKey);
             this.transform.position += Vector3.down;
         }
-        if(Input.GetKeyDown(this.leftKey))
+        if (this.leftKey != null && Input.GetKeyDown(this.leftKey))
         {
             Debug.Log(this.leftKey);
             this.transform.position += Vector3.left;
         }
-        if(Input.GetKeyDown(this.rightKey))
+        if (this.rightKey != null && Input.GetKeyDown(this.rightKey))
         {
             Debug.Log(this.rightKey);
             this.transform.position += Vector3.right;
---
+++ Assets/GGJ2020/Scripts/Parsing/KeyValueFormatParser.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+public class KeyValueFormatParser
+{
+    public static Dictionary<string, string> Parse(string path)
+    {
+        var keyValuePairs = new Dictionary<string, string>();
+
+        if (!File.Exists(path))
+        {
+            return null;
+        }
+
+        using (var reader = new StreamReader(path))
+        {
+            while (reader.Peek() != -1)
+            {
+                ParseLine(reader.ReadLine(), keyValuePairs);
+            }
+        }
+
+        return keyValuePairs;
+    }
+
+    private static void ParseLine(string line, Dictionary<string, string> keyValuePairs)
+    {
+        int equalsIndex = line.IndexOf('=');
+        string key = line.Substring(0, equalsIndex).Trim();
+        string value = line.Substring(equalsIndex + 1).Trim();
+        keyValuePairs[key] = value;
+    }
+}
