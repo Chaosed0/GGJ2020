412261dae7317c9cac258270e73bb45ec0a592e7
chaosed0 <chaosed0@gmail.com>
2020-02-01 11:57:59 -0800

    Movement, animation, collision

--- Assets/GGJ2020/Scripts/Player/Player.cs
+++ Assets/GGJ2020/Scripts/Player/Player.cs
@@ -1,7 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
+
+public enum Direction
+{
+    Left,
+    Right,
+    Down,
+    Up
+}
 
 public class Player : MonoBehaviour
 {
-    public Vector2Int position { get; private set; } = Vector2Int.zero;
+    public UnityAction<Vector3, Vector3> OnPositionChanged = null;
+    public UnityAction<Vector3, Vector3> OnPositionRejected = null;
+
+    private Vector2 DirectionToVector(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Down:
+                return Vector2.down;
+            case Direction.Up:
+                return Vector2.up;
+            case Direction.Right:
+                return Vector2.right;
+            case Direction.Left:
+                return Vector2.left;
+        }
+
+        return Vector2.zero;
+    }
+
+    public bool TryMove(Direction direction)
+    {
+        var vector = DirectionToVector(direction);
+        var hit = Physics2D.Raycast(this.transform.position, vector, 1f);
+
+        var oldPosition = this.transform.position;
+        var newPosition = this.transform.position + Util.three(vector);
+
+        if (hit.collider == null)
+        {
+            this.transform.position = newPosition;
+            OnPositionChanged?.Invoke(oldPosition, newPosition);
+            return true;
+        }
+        else
+        {
+            OnPositionRejected?.Invoke(oldPosition, newPosition);
+            return false;
+        }
+    }
 }
--- Assets/GGJ2020/Scripts/Player/PlayerAnimation.cs
+++ Assets/GGJ2020/Scripts/Player/PlayerAnimation.cs
@@ -1,6 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteRenderer spriteRenderer = null;
+
+    [SerializeField]
+    private Player player = null;
+
+    [SerializeField]
+    private AnimationCurve moveAnimationCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+    [SerializeField]
+    private AnimationCurve rejectAnimationCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+
+    [SerializeField]
+    private float animationTime = 0.25f;
+
+    private Coroutine coroutine = null;
+
+    private void OnValidate()
+    {
+        this.Autofill(ref spriteRenderer, true);
+        this.Autofill(ref player, true, AutofillMode.LookInParents);
+    }
+
+    private void Start()
+    {
+        player.OnPositionChanged += OnPositionChanged;
+        player.OnPositionRejected += OnPositionRejected;
+    }
+
+    private void OnPositionChanged(Vector3 oldPosition, Vector3 newPosition)
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        coroutine = StartCoroutine(AnimateCoroutine(oldPosition, newPosition, moveAnimationCurve));
+    }
+
+    private void OnPositionRejected(Vector3 oldPosition, Vector3 newPosition)
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        coroutine = StartCoroutine(AnimateCoroutine(oldPosition, newPosition, rejectAnimationCurve));
+    }
+
+    private IEnumerator AnimateCoroutine(Vector3 oldPosition, Vector3 newPosition, AnimationCurve animationCurve)
+    {
+        float elapsed = 0f;
+        while (elapsed < animationTime)
+        {
+            float lerp = animationCurve.Evaluate(elapsed / animationTime);
+            spriteRenderer.transform.position = Vector3.Lerp(oldPosition, newPosition, lerp);
+
+            yield return null;
+
+            elapsed += Time.deltaTime;
+        }
+    }
 }
