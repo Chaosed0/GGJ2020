aae696f76dcbc67a690d61d8bd8d1fce90f3d286
chaosed0 <chaosed0@gmail.com>
2020-02-01 23:14:42 -0800

    Log the actual message in the editor

--- Assets/GGJ2020/Scripts/WinCollider.cs
+++ Assets/GGJ2020/Scripts/WinCollider.cs
@@ -45,7 +45,7 @@ public class WinCollider : MonoBehaviour
             }
             else
             {
-                Debug.Log("Crash!");
+                Debug.Log(message);
             }
 
             return;
