29f3ccf66d2d81e6552b12d3250ed728429902c6
chaosed0 <chaosed0@gmail.com>
2020-02-01 23:05:03 -0800

    Tiny particle polish

--- Assets/GGJ2020/Scripts/WinCollider.cs
+++ Assets/GGJ2020/Scripts/WinCollider.cs
@@ -6,6 +6,7 @@ using UnityEngine.Diagnostics;
 public class WinCollider : MonoBehaviour
 {
     private const string videoLocation = "Data/Video.mp4";
+    private const string crashDataLocation = "crash.log";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,16 +19,21 @@ public class WinCollider : MonoBehaviour
 
     private void Win()
     {
-
         var videoPath = MetaLoadUtil.GetPath(videoLocation);
         if (!File.Exists(videoPath))
         {
             //tinyfd.tinyfd_beep();
             //tinyfd.tinyfd_messageBox("Error!", "Error!", "ok", "error", 1);
 
+            var message = $"Victory video not found in {videoPath}! (TODD SAYS TO GET IT FROM https://chaosed0.github.io/GGJ2020/TestEnd.video)";
+            using (var writer = new StreamWriter(crashDataLocation, false))
+            {
+                writer.Write(message);
+            }
+
             if (!Application.isEditor)
             {
-                Debug.LogError($"Video file not found in {videoPath}!");
+                Debug.LogError(message);
                 Utils.ForceCrash(ForcedCrashCategory.Abort);
             }
             else
